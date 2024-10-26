2bd4500a1a165e5bcd38e90164cd89ac35b74da7
dilippatnana <lcb2021030@iiitl.ac.in>
2024-10-26 16:48:54 +0530

    working on hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const heroSection = () => {
+  return (
+    <div className=' pb-20 pt-36'>
+      <div>
+        <h1 className=' font-bold text-[2rem]'>Hero Section</h1>
+      </div>
+    </div>
+  )
+}
+
+export default heroSection 
