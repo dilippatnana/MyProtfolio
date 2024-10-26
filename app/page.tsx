f14099c806e3e2227f2b916ef01d3b8e4d0aa9a0
dilippatnana <lcb2021030@iiitl.ac.in>
2024-10-26 16:51:54 +0530

    add variables in globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
         <Spotlight className="-top-40 -left-10 md:-left-32 md:top-20 h-screen fill-white" />
         <Spotlight className="top-10 left-full h-[80vh] w-[50vw] fill-purple-500 " />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw] fill-blue-600" />
-        {/* <Spotlight className="-top-40 -left-10 md:-left-32 md:top-20 h-screen fill-white" /> */}
         <Hero />
       </div>
     </main>
